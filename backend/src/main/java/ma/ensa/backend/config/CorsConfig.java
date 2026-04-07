8d5b6363103a8d4c9553c3a9403d082654d9f95a
Soufiane Benseddiq <soufianebenseddiq8@gmail.com>
2026-04-07 13:35:57 +0100

    add CI/CD pipeline

--- backend/src/main/java/ma/ensa/backend/config/CorsConfig.java
+++ backend/src/main/java/ma/ensa/backend/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "http://3.236.208.61:4200"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
